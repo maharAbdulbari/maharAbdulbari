dcfae74fbbe96de2653648d3a92ed8fe61a3e4aa
AbdulBari <maharabdulbari1@gmail.com>
2024-05-15 23:15:32 -0700

    add files

---
+++ java1.js
@@ -0,0 +1,63 @@
+// let num2=123;
+// let num3=56;
+
+
+// console.log(num2+ "    " +num3);
+
+// let email="asdgmail.com" ;
+// let abdul="abdul bari" ;
+// let account=78933783267;
+// let etc
+// console.table([email ,abdul ,account ,etc])
+
+// math operators = + - * / divide;
+
+
+//  let data=100 ;
+//  let data1=5;
+// console.log(data / data1)
+
+// let data=100 ;
+//  let data1=5;
+// let add = (data % data1) ;
+// console.log(add);
+
+
+
+// let name = prompt( "what is your  ......" );
+// console.log (name);
+// document.write(name);
+
+
+
+
+// let name = prompt( "what is your  ......" );
+// console.log (name);
+
+// let rendomNumbwer =math.rendom();
+// console.log(rendomNumbwer);
+
+
+
+// let rendomnNmbwer =math.rendom();
+// rendomNumbwer = rendomNumbWer *10;
+// console.log(rendomnumbwer);
+
+// bodmas = bracket open divided multiplication addition subtraction;
+
+
+// let num = 5 + 5 * 5 ;
+// console.log(num)
+
+
+
+// let x =33;
+// console.log(x);
+// // console.log(typeof(x));
+
+let nam = "";
+console.log(typeof(nam));
+
+let x = 33 ;
+let y = true;
+console.log(x + 12 +y);
